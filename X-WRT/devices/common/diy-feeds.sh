db1e508e97fb0f5b2fbec95f7da62691bb0cd0ff
White12352 <86940955+White12352@users.noreply.github.com>
2022-06-20 09:36:15 +0800

    Update diy-feeds.sh

--- X-WRT/devices/common/diy-feeds.sh
+++ X-WRT/devices/common/diy-feeds.sh
@@ -2,3 +2,4 @@
 echo 'src-git zhuxiaole https://github.com/zhuxiaole/OpenWrt-Packages;X-WRT' >>feeds.conf.default
 echo 'src-git onlyme https://github.com/zhuxiaole/my-packages;master' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
