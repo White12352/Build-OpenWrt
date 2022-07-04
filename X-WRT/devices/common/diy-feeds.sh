591492570c73498fac65425001870b600ce3669c
White12352 <86940955+White12352@users.noreply.github.com>
2022-07-04 10:17:54 +0800

    Update diy-feeds.sh

--- X-WRT/devices/common/diy-feeds.sh
+++ X-WRT/devices/common/diy-feeds.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-echo 'src-git zhuxiaole https://github.com/zhuxiaole/OpenWrt-Packages;X-WRT' >>feeds.conf.default
-echo 'src-git onlyme https://github.com/zhuxiaole/my-packages;master' >>feeds.conf.default
+#echo 'src-git zhuxiaole https://github.com/zhuxiaole/OpenWrt-Packages;X-WRT' >>feeds.conf.default
+#echo 'src-git onlyme https://github.com/zhuxiaole/my-packages;master' >>feeds.conf.default
+echo 'src-git opkgsw https://github.com/White12352/OpenWrt-Packages-1;X-WRT' >>feeds.conf.default
+echo 'src-git odys https://github.com/White12352/my-packages;master' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
