de032398e62d65cbac97519feb62ca402e56acc0
White12352 <86940955+White12352@users.noreply.github.com>
2022-07-04 10:54:17 +0800

    Update diy-part2.sh

--- X-WRT/devices/common/diy-part2.sh
+++ X-WRT/devices/common/diy-part2.sh
@@ -31,12 +31,22 @@ sed -i "s/timezone='UTC'/timezone='CST-8'/" package/base-files/files/bin/config_
 rm -rf package/base-files/files/etc/banner
 cp $GITHUB_WORKSPACE/$OP_MANUFACTURER/devices/common/banner package/base-files/files/etc/
 
-rm -rf package/feeds/zhuxiaole/luci-theme-argon/htdocs/luci-static/argon/favicon.ico
-rm -rf package/feeds/zhuxiaole/luci-theme-argon/htdocs/luci-static/argon/img/argon.svg
-rm -rf package/feeds/zhuxiaole/luci-theme-argon/htdocs/luci-static/argon/icon/android-icon-*.png
-rm -rf package/feeds/zhuxiaole/luci-theme-argon/htdocs/luci-static/argon/icon/apple-icon-*.png
-rm -rf package/feeds/zhuxiaole/luci-theme-argon/htdocs/luci-static/argon/icon/favicon-*.png
-rm -rf package/feeds/zhuxiaole/luci-theme-argon/htdocs/luci-static/argon/icon/ms-icon-*.png
-cp $GITHUB_WORKSPACE/$OP_MANUFACTURER/devices/common/argon/favicon.ico package/feeds/zhuxiaole/luci-theme-argon/htdocs/luci-static/argon/
-cp $GITHUB_WORKSPACE/$OP_MANUFACTURER/devices/common/argon/argon.svg package/feeds/zhuxiaole/luci-theme-argon/htdocs/luci-static/argon/img/
-cp $GITHUB_WORKSPACE/$OP_MANUFACTURER/devices/common/argon/icon/* package/feeds/zhuxiaole/luci-theme-argon/htdocs/luci-static/argon/icon/
+#rm -rf package/feeds/zhuxiaole/luci-theme-argon/htdocs/luci-static/argon/favicon.ico
+#rm -rf package/feeds/zhuxiaole/luci-theme-argon/htdocs/luci-static/argon/img/argon.svg
+#rm -rf package/feeds/zhuxiaole/luci-theme-argon/htdocs/luci-static/argon/icon/android-icon-*.png
+#rm -rf package/feeds/zhuxiaole/luci-theme-argon/htdocs/luci-static/argon/icon/apple-icon-*.png
+#rm -rf package/feeds/zhuxiaole/luci-theme-argon/htdocs/luci-static/argon/icon/favicon-*.png
+#rm -rf package/feeds/zhuxiaole/luci-theme-argon/htdocs/luci-static/argon/icon/ms-icon-*.png
+#cp $GITHUB_WORKSPACE/$OP_MANUFACTURER/devices/common/argon/favicon.ico package/feeds/zhuxiaole/luci-theme-argon/htdocs/luci-static/argon/
+#cp $GITHUB_WORKSPACE/$OP_MANUFACTURER/devices/common/argon/argon.svg package/feeds/zhuxiaole/luci-theme-argon/htdocs/luci-static/argon/img/
+#cp $GITHUB_WORKSPACE/$OP_MANUFACTURER/devices/common/argon/icon/* package/feeds/zhuxiaole/luci-theme-argon/htdocs/luci-static/argon/icon/
+
+rm -rf package/feeds/opkgsw/luci-theme-argon/htdocs/luci-static/argon/favicon.ico
+rm -rf package/feeds/opkgsw/luci-theme-argon/htdocs/luci-static/argon/img/argon.svg
+rm -rf package/feeds/opkgsw/luci-theme-argon/htdocs/luci-static/argon/icon/android-icon-*.png
+rm -rf package/feeds/opkgsw/luci-theme-argon/htdocs/luci-static/argon/icon/apple-icon-*.png
+rm -rf package/feeds/opkgsw/luci-theme-argon/htdocs/luci-static/argon/icon/favicon-*.png
+rm -rf package/feeds/opkgsw/luci-theme-argon/htdocs/luci-static/argon/icon/ms-icon-*.png
+cp $GITHUB_WORKSPACE/$OP_MANUFACTURER/devices/common/argon/favicon.ico package/feeds/opkgsw/luci-theme-argon/htdocs/luci-static/argon/
+cp $GITHUB_WORKSPACE/$OP_MANUFACTURER/devices/common/argon/argon.svg package/feeds/opkgsw/luci-theme-argon/htdocs/luci-static/argon/img/
+cp $GITHUB_WORKSPACE/$OP_MANUFACTURER/devices/common/argon/icon/* package/feeds/opkgsw/luci-theme-argon/htdocs/luci-static/argon/icon/
